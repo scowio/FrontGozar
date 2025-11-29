0000e9a3b090cbd74898476630fe0e74632df5e1
Milaadmg023 <bagerim962@gmail.com>
2025-11-29 10:08:23 +0330

    feat: add Results component to display generated configurations

---
+++ components/result.jsx
@@ -0,0 +1,28 @@
+import ConfigBox from "./config-box";
+
+export default function Results({ configs }) {
+  return (
+    <div className="flex flex-col items-center justify-center bg-[#181A20] text-white p-[24px]">
+      <div className="flex flex-col items-start gap-[8px] my-[16px]">
+        <span>Configs generated successfully !</span>
+        <span>Quick Add only available for v2rayNG & Streisand .</span>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <button className="flex items-center justify-center bg-linear-to-bl from-[#335EF7] to-[#5F82FF] py-[11px] px-[48px] rounded-[12px]">
+          Test Again
+        </button>
+        <button className="flex items-center justify-center bg-white text-black py-[11px] px-[48px] rounded-[12px]">
+          Regenerate
+        </button>
+        <button className="col-span-2 flex items-center justify-center bg-linear-to-bl from-[#335EF7] to-[#5F82FF] py-[11px] px-[48px] rounded-[12px]">
+          Sorting By Test Results
+        </button>
+      </div>
+      <div className="overflow-y-scroll flex flex-col gap-[24px]">
+        {configs.map((config, index) => (
+          <ConfigBox config={config} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
